e8958666befd6a7fa272901cfd1cdd5e8e5b3a53
Mateus Sousa <mateus.sousa.silva1512@gmail.com>
2025-10-06 08:17:53 -0300

    Subindo a parte do banco de dados

---
+++ banco_prova_modulo2.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+# Passo 1 - Conectar/criar o banco
+
+
+# Passo 2 - Criar tabela
+
+
+# Passo 3 - Inserir dados
+
+
+
+
+
+# Passo 4 - Listar todos
+
+
+# Passo 5 - Atualizar um registro
+
+
+
+
+# Passo 6 - Deletar um registro
+
+
+
+# Encerrar conexão
